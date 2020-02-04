1cacad7a4d8e040fc9d2f61dd01c19ff5a5133f7
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-04 14:27:14 +0100

    Initial commit of ldorau/libpmemnew

---
+++ src/README.md
@@ -0,0 +1,15 @@
+pmemnew	{#mainpage}
+===========================
+
+pmemnew
+
+All general information about (lib)pmemnew can be found on webstie:
+https://pmem.io/pmemnew
+
+Here user can found description of pmemnew API.
+
+### Important classes/functions ###
+
+ * class pmemnew with its functions:
+	* pmemnew_open() to create/open file with data
+	* pmemnew_close() to close file with data
---
+++ src/libpmemnew.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017-2020, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef LIBPMEMNEW_H
+#define LIBPMEMNEW_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define PMEMNEW_STATUS_OK 0
+#define PMEMNEW_STATUS_UNKNOWN_ERROR 1
+#define PMEMNEW_STATUS_NOT_FOUND 2
+#define PMEMNEW_STATUS_NOT_SUPPORTED 3
+#define PMEMNEW_STATUS_INVALID_ARGUMENT 4
+
+int pmemnew_open(void);
+void pmemnew_close(void);
+const char *pmemnew_errormsg(void);
+
+#endif /* LIBPMEMNEW_H */
